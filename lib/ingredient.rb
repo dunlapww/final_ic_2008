54ea159c320d453d5b485b03652309a809943d4d
Will Dunlap <dunlapww@gmail.com>
2020-09-23 09:05:19 -0600

    Pass test it exists

---
+++ lib/ingredient.rb
@@ -0,0 +1,9 @@
+class Ingredient
+
+  def initialize(details)
+    @name = details[:name]
+    @unit = details[:unit]
+    @calories = details[:calories]
+  end
+
+end
--- test/ingredient_test.rb
+++ test/ingredient_test.rb
@@ -6,6 +6,6 @@ class IngredientTest < Minitest::Test
 
   def test_it_exists
     ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
-    assert_equal Ingredient, ingredient1
+    assert_instance_of Ingredient, ingredient1
   end
 end
